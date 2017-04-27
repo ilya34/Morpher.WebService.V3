741d2f8e0ea4b2267f51ec21ec0eaf5127d7155d
Sergey Filenko <srgFilenko@gmail.com>
2017-04-27 15:24:14 +0300

    Tests

--- Morpher.WebApi.UnitTest/RussianAnalyzerControllerTests.cs
+++ Morpher.WebApi.UnitTest/RussianAnalyzerControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Morpher.WebApi.UnitTest
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Net.Http;
     using System.Web.Http;
     using System.Web.Http.Hosting;
@@ -11,11 +12,13 @@
     using Morpher.WebApi.ApiThrottler;
     using Morpher.WebApi.Controllers;
     using Morpher.WebApi.Models;
+    using Morpher.WebApi.Models.Exceptions;
     using Morpher.WebApi.Services.Interfaces;
 
     using NUnit.Framework;
 
     [TestFixture]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class RussianAnalyzerControllerTests
     {
         [Test]
@@ -34,11 +37,11 @@
 
             RussianAnalyzerController analyzerController =
                 new RussianAnalyzerController(analyzer, apiThrottler, log)
-                    {
-                        Request = new HttpRequestMessage(
+                {
+                    Request = new HttpRequestMessage(
                             HttpMethod.Get,
                             $"http://localhost:0/russian/declension?s=any&token={default(Guid)}")
-                    };
+                };
             analyzerController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
 
             HttpResponseMessage responseMessage = analyzerController.Declension("any");
@@ -47,6 +50,72 @@
             responseMessage.TryGetContentValue(out declensionResult);
 
             Assert.NotNull(declensionResult, "RussianDeclensionResult == null");
+        }
+
+        [Test]
+        public void Declension_Error_Overlimit()
+        {
+            bool paidUser;
+            IApiThrottler apiThrottler = Mock.Of<IApiThrottler>(
+                throttler => throttler.Throttle(It.IsAny<HttpRequestMessage>(), out paidUser)
+                             == ApiThrottlingResult.Overlimit);
+
+            IMorpherLog log = Mock.Of<IMorpherLog>();
+
+            IRussianAnalyzer analyzer = Mock.Of<IRussianAnalyzer>(
+                russianAnalyzer => russianAnalyzer.Declension(It.IsAny<string>(), It.IsAny<Guid>(), It.IsAny<bool>())
+                                   == new RussianDeclensionResult());
+
+            RussianAnalyzerController analyzerController =
+                new RussianAnalyzerController(analyzer, apiThrottler, log)
+                {
+                    Request = new HttpRequestMessage(
+                            HttpMethod.Get,
+                            $"http://localhost:0/russian/declension?s=any&token={default(Guid)}")
+                };
+            analyzerController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+            HttpResponseMessage responseMessage = analyzerController.Declension("any");
+
+            ServiceErrorMessage serviceErrorMessage;
+            responseMessage.TryGetContentValue(out serviceErrorMessage);
+
+            Assert.AreEqual(new ServiceErrorMessage(new ExceededDailyLimitException()), serviceErrorMessage);
+        }
+
+        public void Declension_Error_WordsNotFound()
+        {
+            bool paidUser;
+            IApiThrottler apiThrottler = Mock.Of<IApiThrottler>(
+                throttler => throttler.Throttle(It.IsAny<HttpRequestMessage>(), out paidUser)
+                             == ApiThrottlingResult.Overlimit);
+
+            IMorpherLog log = Mock.Of<IMorpherLog>();
+            Mock<IRussianAnalyzer> mock = new Mock<IRussianAnalyzer>();
+            mock.Setup(
+                    russianAnalyzer => russianAnalyzer.Declension(
+                        It.IsAny<string>(),
+                        It.IsAny<Guid>(),
+                        It.IsAny<bool>()))
+                .Throws(new WordsNotFoundException());
+
+            IRussianAnalyzer analyzer = mock.Object;
+
+            RussianAnalyzerController analyzerController =
+                new RussianAnalyzerController(analyzer, apiThrottler, log)
+                {
+                    Request = new HttpRequestMessage(
+                            HttpMethod.Get,
+                            $"http://localhost:0/russian/declension?s=any&token={default(Guid)}")
+                };
+            analyzerController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+            HttpResponseMessage responseMessage = analyzerController.Declension("any");
+
+            ServiceErrorMessage serviceErrorMessage;
+            responseMessage.TryGetContentValue(out serviceErrorMessage);
+
+            Assert.AreEqual(new ServiceErrorMessage(new ExceededDailyLimitException()), serviceErrorMessage);
         }
     }
 }
--- Morpher.WebApi/Models/ServiceErrorMessage.cs
+++ Morpher.WebApi/Models/ServiceErrorMessage.cs
@@ -1,12 +1,14 @@
 ﻿namespace Morpher.WebApi.Models
 {
+    using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Runtime.Serialization;
 
     using Morpher.WebApi.Models.Exceptions;
 
     [DataContract(Name = "error")]
-    public class ServiceErrorMessage
-    {
+    public class ServiceErrorMessage : IEquatable<ServiceErrorMessage>
+    { 
         public ServiceErrorMessage(MorpherException exception)
         {
             this.Code = exception.Code;
@@ -18,5 +20,45 @@
 
         [DataMember(Name = "message", Order = 1)]
         public string Message { get; protected set; }
+
+        [SuppressMessage("ReSharper", "StyleCop.SA1126")]
+        public static bool operator ==(ServiceErrorMessage left, ServiceErrorMessage right)
+        {
+            return Equals(left, right);
+        }
+
+        [SuppressMessage("ReSharper", "StyleCop.SA1126")]
+        public static bool operator !=(ServiceErrorMessage left, ServiceErrorMessage right)
+        {
+            return !Equals(left, right);
+        }
+
+        [SuppressMessage("ReSharper", "StyleCop.SA1503")]
+        public bool Equals(ServiceErrorMessage other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return this.Code == other.Code && string.Equals(this.Message, other.Message);
+        }
+
+        [SuppressMessage("ReSharper", "StyleCop.SA1503")]
+        [SuppressMessage("ReSharper", "StyleCop.SA1126")]
+        [SuppressMessage("ReSharper", "ArrangeThisQualifier")]
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ServiceErrorMessage)obj);
+        }
+
+        [SuppressMessage("ReSharper", "NonReadonlyMemberInGetHashCode")]
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (this.Code * 397) ^ (this.Message != null ? this.Message.GetHashCode() : 0);
+            }
+        }
     }
 }
