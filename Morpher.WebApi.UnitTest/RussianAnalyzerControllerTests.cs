3a1a3f027d14e86bc810e1d99125483daead04ce
Sergey Filenko <srgFilenko@gmail.com>
2017-05-04 16:57:29 +0300

    some tests

--- Morpher.WebApi.UnitTest/RussianAnalyzerControllerTests.cs
+++ Morpher.WebApi.UnitTest/RussianAnalyzerControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Morpher.WebApi.UnitTest
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Net.Http;
     using System.Web.Http;
@@ -146,6 +147,154 @@
             responseMessage.TryGetContentValue(out numberSpelling);
 
             Assert.NotNull(numberSpelling);
+        }
+
+        [Test]
+        public void Spell_Error_WordsNotFound()
+        {
+            bool paidUser;
+            IApiThrottler apiThrottler = Mock.Of<IApiThrottler>(
+                throttler => throttler.Throttle(It.IsAny<HttpRequestMessage>(), out paidUser)
+                             == ApiThrottlingResult.Success);
+
+            IMorpherLog log = Mock.Of<IMorpherLog>();
+            Mock<IRussianAnalyzer> mock = new Mock<IRussianAnalyzer>();
+            mock.Setup(
+                    russianAnalyzer => russianAnalyzer.Spell(It.IsAny<int>(), It.IsAny<string>()))
+                .Throws(new WordsNotFoundException());
+
+            IRussianAnalyzer analyzer = mock.Object;
+
+            RussianAnalyzerController analyzerController =
+                new RussianAnalyzerController(analyzer, apiThrottler, log)
+                    {
+                        Request = new HttpRequestMessage(
+                            HttpMethod.Get,
+                            $"http://localhost:0/russian/spell?n=10&unit=рубль")
+                    };
+            analyzerController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+            HttpResponseMessage httpResponseMessage = analyzerController.Spell(10, "any");
+            ServiceErrorMessage serviceErrorMessage;
+            httpResponseMessage.TryGetContentValue(out serviceErrorMessage);
+
+            Assert.AreEqual(new ServiceErrorMessage(new WordsNotFoundException()), serviceErrorMessage);
+        }
+
+        [Test]
+        public void Spell_Error_Overlimit()
+        {
+            bool paidUser;
+            IApiThrottler apiThrottler = Mock.Of<IApiThrottler>(
+                throttler => throttler.Throttle(It.IsAny<HttpRequestMessage>(), out paidUser)
+                             == ApiThrottlingResult.Overlimit);
+
+            IMorpherLog log = Mock.Of<IMorpherLog>();
+
+
+            RussianAnalyzerController analyzerController =
+                new RussianAnalyzerController(null, apiThrottler, log)
+                    {
+                        Request = new HttpRequestMessage(
+                            HttpMethod.Get,
+                            $"http://localhost:0/russian/spell?n=10&unit=рубль")
+                    };
+            analyzerController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+            HttpResponseMessage httpResponseMessage = analyzerController.Spell(10, "any");
+            ServiceErrorMessage serviceErrorMessage;
+            httpResponseMessage.TryGetContentValue(out serviceErrorMessage);
+
+            Assert.AreEqual(new ServiceErrorMessage(new ExceededDailyLimitException()), serviceErrorMessage);
+        }
+
+        [Test]
+        public void Adjectivize_Success()
+        {
+            bool paidUser;
+            IApiThrottler apiThrottler = Mock.Of<IApiThrottler>(
+                throttler => throttler.Throttle(It.IsAny<HttpRequestMessage>(), out paidUser)
+                             == ApiThrottlingResult.Success);
+
+            IMorpherLog log = Mock.Of<IMorpherLog>();
+
+            IRussianAnalyzer analyzer = Mock.Of<IRussianAnalyzer>(
+                russianAnalyzer => russianAnalyzer.Adjectives(It.IsAny<string>())
+                                   == new List<string>());
+
+            RussianAnalyzerController analyzerController =
+                new RussianAnalyzerController(analyzer, apiThrottler, log)
+                    {
+                        Request = new HttpRequestMessage(
+                            HttpMethod.Get,
+                            $"http://localhost:0/russian/adjectivize?s=any")
+                    };
+            analyzerController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+            HttpResponseMessage responseMessage = analyzerController.Adjectivize("any");
+            List<string> adjectives;
+            responseMessage.TryGetContentValue(out adjectives);
+
+            Assert.NotNull(adjectives);
+        }
+
+        [Test]
+        public void Adjectivize_Error_Overlimit()
+        {
+            bool paidUser;
+            IApiThrottler apiThrottler = Mock.Of<IApiThrottler>(
+                throttler => throttler.Throttle(It.IsAny<HttpRequestMessage>(), out paidUser)
+                             == ApiThrottlingResult.Overlimit);
+
+            IMorpherLog log = Mock.Of<IMorpherLog>();
+
+            RussianAnalyzerController analyzerController =
+                new RussianAnalyzerController(null, apiThrottler, log)
+                    {
+                        Request = new HttpRequestMessage(
+                            HttpMethod.Get,
+                            $"http://localhost:0/russian/adjectivize?s=any")
+                    };
+            analyzerController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+            HttpResponseMessage responseMessage = analyzerController.Adjectivize("any");
+            ServiceErrorMessage serviceErrorMessage;
+            responseMessage.TryGetContentValue(out serviceErrorMessage);
+
+            Assert.AreEqual(new ServiceErrorMessage(new ExceededDailyLimitException()), serviceErrorMessage);
+        }
+
+        [Test]
+        public void Adjectivize_Error_WordsNotFound()
+        {
+            bool paidUser;
+            IApiThrottler apiThrottler = Mock.Of<IApiThrottler>(
+                throttler => throttler.Throttle(It.IsAny<HttpRequestMessage>(), out paidUser)
+                             == ApiThrottlingResult.Success);
+
+            IMorpherLog log = Mock.Of<IMorpherLog>();
+
+            Mock<IRussianAnalyzer> mock = new Mock<IRussianAnalyzer>();
+            mock.Setup(
+                    russianAnalyzer => russianAnalyzer.Adjectives(It.IsAny<string>()))
+                .Throws(new WordsNotFoundException());
+
+            IRussianAnalyzer analyzer = mock.Object;
+
+            RussianAnalyzerController analyzerController =
+                new RussianAnalyzerController(analyzer, apiThrottler, log)
+                    {
+                        Request = new HttpRequestMessage(
+                            HttpMethod.Get,
+                            $"http://localhost:0/russian/adjectivize?s=any")
+                    };
+            analyzerController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+
+            HttpResponseMessage responseMessage = analyzerController.Adjectivize("any");
+            ServiceErrorMessage serviceErrorMessage;
+            responseMessage.TryGetContentValue(out serviceErrorMessage);
+
+            Assert.AreEqual(new ServiceErrorMessage(new WordsNotFoundException()), serviceErrorMessage);
         }
     }
 }
