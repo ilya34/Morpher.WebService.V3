98acac22fe008d844720966dc555bfa991f142a5
Sergey Slepov <slepov@gmail.com>
2017-08-09 01:24:20 +0500

    Fix indentation.

--- Morpher.WebApi/Services/UserCorrectionDatabaseService.cs
+++ Morpher.WebApi/Services/UserCorrectionDatabaseService.cs
@@ -14,32 +14,32 @@
             using (UserCorrectionDataContext context = new UserCorrectionDataContext())
             {
                 var corrections = context.sp_GetForms(lemma.ToUpperInvariant(), "RU", token.Value).ToList();
-                    foreach (var correction in corrections.Where(form => form.Plural == plural))
+                foreach (var correction in corrections.Where(form => form.Plural == plural))
+                {
+                    switch (correction.FormID.ToString())
                     {
-                        switch (correction.FormID.ToString())
-                        {
-                            case "И":
-                                paradigm.Nominative = correction.AccentedText;
-                                break;
-                            case "Р":
-                                paradigm.Genitive = correction.AccentedText;
-                                break;
-                            case "Д":
-                                paradigm.Dative = correction.AccentedText;
-                                break;
-                            case "В":
-                                paradigm.Accusative = correction.AccentedText;
-                                break;
-                            case "Т":
-                                paradigm.Instrumental = correction.AccentedText;
-                                break;
-                            case "П":
-                                paradigm.Prepositional = correction.AccentedText;
-                                break;
-                            case "М":
-                                paradigm.PrepositionalWithPre = correction.AccentedText;
-                                break;
-                        }
+                        case "И":
+                            paradigm.Nominative = correction.AccentedText;
+                            break;
+                        case "Р":
+                            paradigm.Genitive = correction.AccentedText;
+                            break;
+                        case "Д":
+                            paradigm.Dative = correction.AccentedText;
+                            break;
+                        case "В":
+                            paradigm.Accusative = correction.AccentedText;
+                            break;
+                        case "Т":
+                            paradigm.Instrumental = correction.AccentedText;
+                            break;
+                        case "П":
+                            paradigm.Prepositional = correction.AccentedText;
+                            break;
+                        case "М":
+                            paradigm.PrepositionalWithPre = correction.AccentedText;
+                            break;
+                    }
                 }
             }
                
