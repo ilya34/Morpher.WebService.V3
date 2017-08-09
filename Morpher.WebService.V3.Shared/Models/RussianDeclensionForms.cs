47272cd098aaa83f2c7847e927bf37c9a207707d
Sergey Slepov <slepov@gmail.com>
2017-08-09 16:18:43 +0500

    Remove Equals and GetHashCode from xxDeclensionForms. They are not used.

--- Morpher.WebService.V3.Shared/Models/RussianDeclensionForms.cs
+++ Morpher.WebService.V3.Shared/Models/RussianDeclensionForms.cs
@@ -48,46 +48,5 @@
                 }
             }
         }
-
-        [SuppressMessage("ReSharper", "StyleCop.SA1503")]
-        public bool Equals(RussianDeclensionForms other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return string.Equals(this.Nominative, other.Nominative) &&
-                string.Equals(this.Genitive, other.Genitive) &&
-                string.Equals(this.Dative, other.Dative) &&
-                string.Equals(this.Accusative, other.Accusative) &&
-                string.Equals(this.Instrumental, other.Instrumental) &&
-                string.Equals(this.Prepositional, other.Prepositional) &&
-                string.Equals(this.PrepositionalWithPre, other.PrepositionalWithPre);
-        }
-
-        [SuppressMessage("ReSharper", "StyleCop.SA1503")]
-        [SuppressMessage("ReSharper", "StyleCop.SA1126")]
-        [SuppressMessage("ReSharper", "ArrangeThisQualifier")]
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return this.Equals((RussianDeclensionForms)obj);
-        }
-
-        [SuppressMessage("ReSharper", "StyleCop.SA1119")]
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = (this.Nominative != null ? this.Nominative.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (this.Genitive != null ? this.Genitive.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (this.Dative != null ? this.Dative.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (this.Accusative != null ? this.Accusative.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (this.Instrumental != null ? this.Instrumental.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (this.Prepositional != null ? this.Prepositional.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (this.PrepositionalWithPre != null ? this.PrepositionalWithPre.GetHashCode() : 0);
-                return hashCode;
-            }
-        }
     }
 }
