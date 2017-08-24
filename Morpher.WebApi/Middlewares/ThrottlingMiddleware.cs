76882bf5aa8254ea661b692ec896bc9fc6e3aa7a
Sergey Filenko <sergey@filenko.me>
2017-08-24 14:15:11 +0300

    ws2 больше не работает

--- Morpher.WebApi/Middlewares/ThrottlingMiddleware.cs
+++ Morpher.WebApi/Middlewares/ThrottlingMiddleware.cs
@@ -2,7 +2,6 @@
 {
     using System.IO;
     using System.Net;
-    using System.Net.Http.Formatting;
     using System.Runtime.Serialization;
     using System.Threading.Tasks;
     using Extensions;
