b7afb65294a428c617935e89248620952a5b9dac
Sergey Filenko <sergey@filenko.me>
2017-08-24 19:44:21 +0300

    нужен новый API для работы со словарями

--- Morpher.WebApi/Controllers/RussianAnalyzerController.cs
+++ Morpher.WebApi/Controllers/RussianAnalyzerController.cs
@@ -23,8 +23,6 @@
             _analyzer = analyzer;
         }
 
-        public bool IsLocalService { get; set; }
-
         [Route("declension", Name = "RussianDeclension")]
         [ThrottleThis]
         [LogThis]
@@ -77,126 +75,13 @@
         [HttpGet]
         public HttpResponseMessage AdjectiveGenders(string s, ResponseFormat? format = null)
         {
-            throw new NotImplementedException();
-            //try
-            //{
-            //    if (string.IsNullOrWhiteSpace(s))
-            //    {
-            //        throw new RequiredParameterIsNotSpecified(nameof(s));
-            //    }
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                throw new RequiredParameterIsNotSpecified(nameof(s));
+            }
 
-            //    bool paidUser;
-            //    ApiThrottlingResult result = this.apiThrottler.Throttle(this.Request, out paidUser);
-
-            //    if (result != ApiThrottlingResult.Success)
-            //    {
-            //        throw result.GenerateMorpherException();
-            //    }
-
-            //    AdjectiveGenders adjectives = this.analyzer.AdjectiveGenders(s);
-
-            //    this.morpherLog.Log(this.Request);
-            //    return this.Request.CreateResponse(HttpStatusCode.OK, adjectives, format);
-            //}
-            //catch (MorpherException exception)
-            //{
-            //    this.morpherLog.Log(this.Request, exception);
-            //    return this.Request.CreateResponse(
-            //        HttpStatusCode.BadRequest,
-            //        new ServiceErrorMessage(exception),
-            //        format);
-            //}
-        }
-
-
-        [Route("userdict")]
-        [HttpPost]
-        [SuppressMessage("ReSharper", "InconsistentNaming")]
-        public HttpResponseMessage AddOrUpdateUserCorrection(
-            string и,
-            string р = null,
-            string д = null,
-            string в = null,
-            string т = null,
-            string п = null,
-            string п_о = null,
-            string и_м = null,
-            string р_м = null,
-            string д_м = null,
-            string в_м = null,
-            string т_м = null,
-            string п_м = null,
-            string п_о_м = null,
-            ResponseFormat? format = null)
-        {
-            throw new NotImplementedException();
-            //if (!this.IsLocalService)
-            //{
-            //    return this.Request.CreateResponse(HttpStatusCode.Forbidden, false, format);
-            //}
-
-            //if (string.IsNullOrWhiteSpace(и))
-            //{
-            //    return this.Request.CreateResponse(
-            //        HttpStatusCode.BadRequest,
-            //        new ServiceErrorMessage(new RequiredParameterIsNotSpecified(nameof(и))),
-            //        format);
-            //}
-
-            //RussianEntry russianEntry = new RussianEntry()
-            //{
-            //    Singular = new RussianDeclensionForms()
-            //    {
-            //        Nominative = и,
-            //        Dative = д,
-            //        Genitive = р,
-            //        Instrumental = т,
-            //        Accusative = в,
-            //        Prepositional = п,
-            //        PrepositionalWithPre = п_о
-            //    },
-            //    Plural = new RussianDeclensionForms()
-            //    {
-            //        Nominative = и_м,
-            //        Dative = д_м,
-            //        Genitive = р_м,
-            //        Instrumental = т_м,
-            //        Accusative = в_м,
-            //        Prepositional = п_м,
-            //        PrepositionalWithPre = п_о_м
-            //    }
-            //};
-
-            //this.russianDictService.AddOrUpdate(russianEntry);
-
-            //return this.Request.CreateResponse(HttpStatusCode.OK, true, format);
-        }
-
-        [Route("userdict")]
-        [HttpDelete]
-        public HttpResponseMessage RemoveCorrection(string s, ResponseFormat? format = null)
-        {
-            throw new NotImplementedException();
-            //if (!this.IsLocalService)
-            //{
-            //    return this.Request.CreateResponse(HttpStatusCode.Forbidden, false, format);
-            //}
-
-            //this.russianDictService.Remove(s);
-            //return this.Request.CreateResponse(HttpStatusCode.Forbidden, true, format);
-        }
-
-        [Route("userdict")]
-        [HttpGet]
-        public HttpResponseMessage GetAllCorrections(ResponseFormat? format = null)
-        {
-            throw new NotImplementedException();
-            //if (!this.IsLocalService)
-            //{
-            //    return this.Request.CreateResponse(HttpStatusCode.Forbidden, false, format);
-            //}
-
-            //return this.Request.CreateResponse(HttpStatusCode.OK, this.russianDictService.GetAll(), format);
+            AdjectiveGenders adjectives = _analyzer.AdjectiveGenders(s);
+            return Request.CreateResponse(HttpStatusCode.OK, adjectives, format);
         }
     }
 }
