b03ba17c00211f1533143c6aac607a4e4a1d1daf
Sergey Filenko <sergey@filenko.me>
2017-08-24 15:43:37 +0300

    Exception filter

---
+++ Morpher.WebApi/Filters/MorpherExceptionFilterAttribute.cs
@@ -0,0 +1,79 @@
+﻿namespace Morpher.WebService.V3.Filters
+{
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Runtime.Serialization;
+    using System.Text;
+    using System.Web.Http.Filters;
+    using System.Xml;
+    using Extensions;
+    using Models.Exceptions;
+    using Newtonsoft.Json;
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class MorpherExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            var exception = context.Exception as MorpherException;
+            if (exception == null)
+            {
+                return;
+            }
+
+            var format = context.Request.GetQueryString("format");
+
+            if (format == null)
+            {
+                format = context.Request.GetHeader("ContentType");
+
+                if (format == null)
+                {
+                    format = "xml";
+                }
+                else if (format.Contains("application/json"))
+                {
+                    format = "json";
+                }
+                else if (format.Contains("application/xml"))
+                {
+                    format = "xml";
+                }
+            }
+
+            var response = new Models.ServiceErrorMessage(exception);
+
+            switch (format)
+            {
+                case "json":
+                    context.Response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    context.Response.Content =
+                        new StringContent(
+                            JsonConvert.SerializeObject(response, Formatting.Indented));
+                    break;
+                case "xml":
+                default:
+                    DataContractSerializer contractSerializer = new DataContractSerializer(typeof(Models.ServiceErrorMessage));
+                    // Если писать поток в StreamContent ничего не выводит
+                    // Если из ByteArray то кривая кодировка.
+                    using (StringWriter stringWriter = new StringWriter())
+                    {
+                        using (XmlTextWriter textWriter =
+                            new XmlTextWriter(stringWriter))
+                        {
+                            textWriter.Formatting = System.Xml.Formatting.Indented;
+                            contractSerializer.WriteObject(textWriter, response);
+                            context.Response =
+                                new HttpResponseMessage(HttpStatusCode.BadRequest)
+                                {
+                                    Content = new StringContent(stringWriter.GetStringBuilder().ToString())
+                                };
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
