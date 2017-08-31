97e51005b44d187db00982a770cfde67b1485081
Sergey Filenko <sergey@filenko.me>
2017-08-31 20:14:35 +0300

    lemmanormalizer

---
+++ Morpher.WebApi/General/LemmaNormalizer.cs
@@ -0,0 +1,39 @@
+﻿namespace Morpher.WebService.V3.General
+{
+    using System.Text.RegularExpressions;
+
+    public class LemmaNormalizer
+    {
+        static readonly Regex Regex = new Regex(@"[\s-,.]+", RegexOptions.Compiled);
+
+        public static string Normalize(string s)
+        {
+            string upperInvariant =
+                CompressWhitespace(s)
+                    .Trim();
+
+            return ToUpperRemoveAccents(upperInvariant);
+        }
+
+        public static string ToUpperRemoveAccents(string s)
+        {
+            string x = s
+                .ToUpperInvariant()
+                .Replace('Ё', 'Е');
+
+            return RemoveAccents(x);
+        }
+
+        public static string RemoveAccents(string s)
+        {
+            return s
+                .Replace("\u0300", "")  // combining grave accent 
+                .Replace("\u0301", ""); // combining acute accent 
+        }
+
+        public static string CompressWhitespace(string s)
+        {
+            return Regex.Replace(s, " ");
+        }
+    }
+}
