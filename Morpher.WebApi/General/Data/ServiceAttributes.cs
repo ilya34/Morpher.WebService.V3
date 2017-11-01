7e0c628b8ae975762a87fa9f255bbc4adf1d9f47
Sergey Filenko <sergey@filenko.me>
2017-11-01 20:51:52 +0200

    doc fix

--- Morpher.WebApi/General/Data/ServiceAttributes.cs
+++ Morpher.WebApi/General/Data/ServiceAttributes.cs
@@ -12,14 +12,13 @@
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class ThrottleThisAttribute : Attribute
     {
-
         /// <summary>
         /// For per word tarification use overload with delimiter
         /// </summary>
-        /// <param name="cost">Maximum symbols for one request</param>
+        /// <param name="cost">Cost of request</param>
         /// <param name="mode">TarificationMode</param>
         /// <remarks>
-        /// In PerSymbol mode cost means maximum symbols for one request.
+        /// In PerSymbol mode cost means quantity of symbols that will count as one request.
         /// Example: If cost = 50 then 45 symbols = 1 request, 54 = 2 requests.
         /// Symbols count always rounded to upper bound.
         /// </remarks>
@@ -32,6 +31,7 @@
         /// <summary>
         /// Used for PerWord &amp; PerSymbol tarification mode
         /// </summary>
+        /// <param name="cost">Cost of one word</param>
         /// <param name="delimiter">symbol for delimiter</param>
         public ThrottleThisAttribute(int cost, char delimiter)
         {
