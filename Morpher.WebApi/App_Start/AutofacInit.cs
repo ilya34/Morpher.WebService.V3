cc13c5ec5c8f5e98dc690781ac773ebb455fa078
Sergey Filenko <sergey@filenko.me>
2017-12-19 00:15:45 +0200

    more configs

--- Morpher.WebApi/App_Start/AutofacInit.cs
+++ Morpher.WebApi/App_Start/AutofacInit.cs
@@ -4,19 +4,14 @@ namespace Morpher.WebService.V3
 {
     using System;
     using System.Configuration;
-    using System.IO;
     using System.Reflection;
-    using System.Web.Hosting;
     using System.Web.Http;
     using Autofac;
     using Autofac.Core;
     using Autofac.Integration.WebApi;
     using General.Data;
-    using General.Data.Interfaces;
     using General.Data.Middlewares;
     using General.Data.Services;
-    using Russian;
-    using Ukrainian;
 
     public static class AutofacInit
     {
@@ -34,7 +29,7 @@ namespace Morpher.WebService.V3
             builder.RegisterWebApiModelBinderProvider();
 
             RegisterServices(builder);
-            builder.RegisterModule(new ConfigurationSettingsReader("autofac", "InjectionConfigs/Local.config"));
+            builder.RegisterModule(new ConfigurationSettingsReader("autofac", "InjectionConfigs/WS3.config"));
             Container = builder.Build();
             AutofacWebApiDependencyResolver = new AutofacWebApiDependencyResolver(Container);
             config.DependencyResolver = AutofacWebApiDependencyResolver;
@@ -42,94 +37,21 @@ namespace Morpher.WebService.V3
 
         private static void RegisterServices(ContainerBuilder builder)
         {
-
             bool runAsLocalService = Convert.ToBoolean(ConfigurationManager.AppSettings.Get("RunAsLocalService"));
 
-
-            if (runAsLocalService)
-            {
-                RegisterLocalOnlyServices(builder);
-
-                //string externalAnalyzer = ConfigurationManager.AppSettings.Get("ExternalAnalyzer");
-                //string path = Path.Combine(
-                //    AppDomain.CurrentDomain.BaseDirectory,
-                //    "bin",
-                //    externalAnalyzer);
-                //var analyzer = Assembly.LoadFile(path);
-                //string filePathRu = HostingEnvironment.MapPath("~/App_Data/UserDict.xml");
-                //string filePathUkr = HostingEnvironment.MapPath("~/App_Data/UserDictUkr.xml");
-                //builder.RegisterAssemblyTypes(analyzer)
-                //    .Where(type => typeof(Russian.IExceptionDictionary).IsAssignableFrom(type))
-                //    .As<Russian.IExceptionDictionary, Russian.IUserDictionaryLookup>().SingleInstance().WithParameter("userDict", filePathRu);
-                //builder.RegisterAssemblyTypes(analyzer)
-                //    .Where(type => typeof(Ukrainian.IExceptionDictionary).IsAssignableFrom(type))
-                //    .As<Ukrainian.IExceptionDictionary, Ukrainian.IUserDictionaryLookup>().SingleInstance().WithParameter("userDict", filePathUkr);
-            }
-            else
+            if (!runAsLocalService)
             {
                 RegisterGlobalOnlyServices(builder);
-
-                builder.RegisterType<MorpherCache>()
-                    .As<ICorrectionCache>()
-                    .WithParameter("name", "UserCorrection")
-                    .SingleInstance();
-
-
-                builder.RegisterType<DatabaseUserDictionary>()
-                    .As<Russian.IUserDictionaryLookup, Ukrainian.IUserDictionaryLookup>()
-                    .As<Russian.IExceptionDictionary, Ukrainian.IExceptionDictionary>().SingleInstance();
             }
 
             RegisterSharedServices(builder);
-            //RegisterAnalyzers(builder);
         }
-
-        private static void RegisterAnalyzers(ContainerBuilder builder)
-        {
-            string externalAnalyzer = ConfigurationManager.AppSettings.Get("ExternalAnalyzer");
-
-            if (externalAnalyzer == null)
-            {
-                MorpherClient client = new MorpherClient();
-                builder.RegisterType<RussianWebAnalyzer>().As<IRussianAnalyzer>()
-                    .WithParameter("client", client.Russian)
-                    .SingleInstance();
-                builder.RegisterType<UkrainianWebAnalyzer>().As<IUkrainianAnalyzer>()
-                    .WithParameter("client", client.Ukrainian)
-                    .SingleInstance();
-            }
-            else
-            {
-                string path = Path.Combine(
-                    AppDomain.CurrentDomain.BaseDirectory,
-                    "bin",
-                    externalAnalyzer);
-                var analyzer = Assembly.LoadFile(path);
-
-                builder.RegisterAssemblyTypes(analyzer)
-                    .Where(type => typeof(IRussianAnalyzer).IsAssignableFrom(type))
-                    .As<IRussianAnalyzer>().SingleInstance();
-                builder.RegisterAssemblyTypes(analyzer)
-                    .Where(type => typeof(IUkrainianAnalyzer).IsAssignableFrom(type))
-                    .As<IUkrainianAnalyzer>().SingleInstance();
-            }
-        }
-
+        
         private static void RegisterSharedServices(ContainerBuilder builder)
         {
-            builder.RegisterType<MorpherCache>()
-                .As<IMorpherCache>()
-                .WithParameter("name", "ApiThrottler")
-                .SingleInstance();
-
             // Filters
             builder.Register(context => new MorpherExceptionFilterAttribute())
                 .AsWebApiExceptionFilterFor<ApiController>().SingleInstance();
-        }
-
-        private static void RegisterLocalOnlyServices(ContainerBuilder builder)
-        {
-            builder.RegisterType<DummyResultTrimmer>().As<IResultTrimmer>();
         }
 
         private static void RegisterGlobalOnlyServices(ContainerBuilder builder)
@@ -153,16 +75,13 @@ namespace Morpher.WebService.V3
                 .As<IResultTrimmer>();
 
 
-            builder.RegisterType<LogSyncer>().AsSelf().InstancePerLifetimeScope();
-            builder.RegisterType<UserCacheSyncer>().AsSelf().InstancePerLifetimeScope();
-
             // Используется в LoggingMiddleware
-            builder.RegisterType<MorpherLog>().As<IMorpherLog>().SingleInstance();
+            //builder.RegisterType<MorpherLog>().As<IMorpherLog>().SingleInstance();
             builder.RegisterType<DatabaseLog>().As<IDatabaseLog>()
                 .WithParameter("connectionString", connectionString);
 
             // Исплользуется в ThrottlingMIddleware
-            builder.RegisterType<ApiThrottler>().As<IApiThrottler>();
+            //builder.RegisterType<ApiThrottler>().As<IApiThrottler>();
             builder.RegisterType<MorpherDatabase>().As<IMorpherDatabase>()
                 .WithParameter("connectionString", connectionString);
 
