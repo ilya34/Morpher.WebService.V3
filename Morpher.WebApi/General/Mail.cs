34f219bb1e9936be50ea0ce13452e4324ccdd600
Sergey Filenko <sergey@filenko.me>
2017-12-26 22:37:46 +0200

    forgot to push this file

---
+++ Morpher.WebApi/General/Mail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Morpher.WebService.V3.General
+{
+    public static class Mail
+    {
+        public static void SendErrorEmail(Exception e)
+        {
+            var client = new SmtpClient();
+            client.Send("robot2@morpher.ru", "support@morpher.ru", "ws3 exception", e.Message);
+        }
+    }
+}
