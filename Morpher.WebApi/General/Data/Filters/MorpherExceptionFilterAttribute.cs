2b44c638a270d9165162ae0821ac6792600d4f4b
Sergey Filenko <sergey@filenko.me>
2017-12-28 13:28:20 +0200

    refactor

--- Morpher.WebApi/General/Data/Filters/MorpherExceptionFilterAttribute.cs
+++ Morpher.WebApi/General/Data/Filters/MorpherExceptionFilterAttribute.cs
@@ -1,72 +1,64 @@
-﻿using System.Net.Http.Headers;
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Web.Http.Filters;
+using System.Net.Http.Headers;
+using System.Xml.Serialization;
+using Autofac.Integration.WebApi;
+using Morpher.WebService.V3.General.Data.Exceptions;
 
 namespace Morpher.WebService.V3.General.Data
 {
-    using System.IO;
-    using System.Net;
-    using System.Net.Http;
-    using System.Runtime.Serialization;
-    using System.Web.Http.Filters;
-    using System.Xml;
-    using Autofac.Integration.WebApi;
-    using Exceptions;
+
     using Newtonsoft.Json;
-    using Formatting = Newtonsoft.Json.Formatting;
+
 
     public class MorpherExceptionFilterAttribute : ExceptionFilterAttribute, IAutofacExceptionFilter
     {
+        private ResponseFormat GetResponseFormat(HttpActionExecutedContext context)
+        {
+            var requestedResponseFormat =
+                context.Request.GetQueryString("format") ?? context.Request.GetHeader("Accept");
+            ResponseFormat responseFormat = ResponseFormat.Xml;
+            if (requestedResponseFormat != null
+                && (requestedResponseFormat.ToLowerInvariant() == "json"
+                    || requestedResponseFormat.Contains("application/json")))
+                responseFormat = ResponseFormat.Json;
+            return responseFormat;
+        }
+
         public override void OnException(HttpActionExecutedContext context)
         {
             var exception = context.Exception as MorpherException ?? new ServerException(context.Exception);
+            var responseFormat = GetResponseFormat(context);
 
-            var format = context.Request.GetQueryString("format") ?? context.Request.GetHeader("Accept");
-
-            if (format == null)
-            {
-                format = "xml";
-            }
-            else if (format.Contains("application/json"))
-            {
-                format = "json";
-            }
-            else if (format.Contains("application/xml"))
-            {
-                format = "xml";
-            }
-
-            var response = new ServiceErrorMessage(exception);
-            context.Response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            var responseObject = new ServiceErrorMessage(exception);
+            context.Response = new HttpResponseMessage();
             if (!context.Response.Headers.Contains("Error-Code"))
             {
-                context.Response.Headers.Add("Error-Code", new[] {response.Code.ToString()});
+                context.Response.Headers.Add("Error-Code", new[] { responseObject.Code.ToString() });
                 context.Response.StatusCode = exception.ResponseCode;
             }
 
-            switch (format)
+            switch (responseFormat)
             {
-                case "json":
+                case ResponseFormat.Json:
                     context.Response.Content =
                         new StringContent(
-                            JsonConvert.SerializeObject(response, Formatting.Indented));
+                            JsonConvert.SerializeObject(responseObject, Formatting.Indented));
                     context.Response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
                     break;
-                case "xml":
-                default:
-                    DataContractSerializer contractSerializer = new DataContractSerializer(typeof(ServiceErrorMessage));
-                    // Если писать поток в StreamContent ничего не выводит
-                    // Если из ByteArray то кривая кодировка.
+                case ResponseFormat.Xml:
+                    XmlSerializer contractSerializer = new XmlSerializer(typeof(ServiceErrorMessage));
                     using (StringWriter stringWriter = new StringWriter())
                     {
-                        using (XmlTextWriter textWriter =
-                            new XmlTextWriter(stringWriter))
-                        {
-                            textWriter.Formatting = System.Xml.Formatting.Indented;
-                            contractSerializer.WriteObject(textWriter, response);
-                            context.Response.Content = new StringContent(stringWriter.GetStringBuilder().ToString());
-                        }
+                        contractSerializer.Serialize(stringWriter, responseObject);
+                        context.Response.Content = new StringContent(stringWriter.GetStringBuilder().ToString());
                     }
                     context.Response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/xml");
                     break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
