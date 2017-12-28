5bc72bc903d89637ac1c5729ca256f86c46c7f4a
Sergey Filenko <sergey@filenko.me>
2017-12-28 13:54:44 +0200

    log databse logger errors

---
+++ Morpher.WebApi/General/Data/Mail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Morpher.WebService.V3.General.Data
+{
+    public static class Mail
+    {
+        public static void SendErrorEmail(Exception e)
+        {
+            var client = new SmtpClient();
+            client.Send("robot2@morpher.ru", "support@morpher.ru", "ws3 exception", e.ToString());
+        }
+    }
+}
