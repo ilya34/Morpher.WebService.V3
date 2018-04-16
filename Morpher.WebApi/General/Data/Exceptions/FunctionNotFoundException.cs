36f2bf0aebc0e14f0ce63b875ac5838bedb288b2
ilya34 <38351928+ilya34@users.noreply.github.com>
2018-04-16 22:23:27 +0300

    Add files via upload

---
+++ Morpher.WebApi/General/Data/Exceptions/FunctionNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿namespace Morpher.WebService.V3.General.Data
+{
+    using System;
+    using System.Net;
+
+    public class FunctionNotFoundException : MorpherException
+    {
+        private static readonly string ErrorMessage = "Запрошенная функция не поддерживается";
+
+        private static readonly HttpStatusCode ResponseWith = HttpStatusCode.NotImplemented;//http code = 501 // custom code = 493;
+
+        public FunctionNotFoundException()
+            : base(ErrorMessage, 13)
+        {
+            Code = 13;
+            ResponseCode = ResponseWith;
+        }
+
+        public FunctionNotFoundException(string mesage, int code = 13, Exception innerException = null)
+            : base(mesage, code, innerException)
+        {
+            Code = code;
+            ResponseCode = ResponseWith;
+        }
+    }
+}
